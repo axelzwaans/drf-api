5b00e9ccf1326167deda448496d759cb79c754d8
Axel Zwaans <axelzwaans@hotmail.com>
2023-02-24 11:29:44 +0000

    add profiles detail view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -19,7 +19,8 @@ if os.path.exists('env.py'):
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
-
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -47,7 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-
+    'rest_framework',
     'profiles'
 ]
 
