648296bfc99a137693e53422ea8799e32a668d14
Axel Zwaans <axelzwaans@hotmail.com>
2023-03-02 09:38:41 +0000

    Set debug to false

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,8 +56,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+
 
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
